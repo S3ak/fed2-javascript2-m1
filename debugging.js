a185cee54787d0a3527b4a6ca6db73a2186125a8
Monde Sineke <nsineke@gmail.com>
2023-09-04 18:45:01 +0200

    ✨ feat(debugginh): create example

---
+++ debugging.js
@@ -0,0 +1,26 @@
+function complexCalculation() {
+  for (let i = 0; i < 1000; i++) {
+    console.log(i);
+  }
+}
+
+// Run three separate tests below
+
+console.time("myTimer");
+complexCalculation();
+console.timeEnd("myTimer");
+// Logs:
+// myTimer: 101ms - timer ended
+
+console.time("myTimer");
+complexCalculation();
+console.timeEnd("myTimer");
+// Logs:
+// myTimer: 155ms - timer ended
+
+console.time("myTimer");
+complexCalculation();
+console.timeEnd("myTimer");
+// Logs:
+// myTimer: 128ms - timer ended
+1;
